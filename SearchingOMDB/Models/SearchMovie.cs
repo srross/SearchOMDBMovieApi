69f0d00f96a799782acb049733595746bae48482
SoniaRoss <srross99@gmail.com>
2022-05-14 21:54:23 -0400

    bootstrapping a pinch of pretty

--- SearchingOMDB/Models/SearchMovie.cs
+++ SearchingOMDB/Models/SearchMovie.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SearchingOMDB.Models
 {
     public class SearchMovie
     {
         [Required]
+        [DisplayName("Movie Title")]
         public string MovieTitle { get; set; }
 
         // could add other search types beyond movie title
--- SearchingOMDB/Models/SearchMovieList.cs
+++ SearchingOMDB/Models/SearchMovieList.cs
@@ -1,9 +1,19 @@
-﻿namespace SearchingOMDB.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SearchingOMDB.Models
 {
     public class SearchMovieList
     {
+        [Required]
+        [DisplayName("Movie Title 1")]
         public string MovieTitle1 { get; set; }
+
+        [Required]
+        [DisplayName("Movie Title 2 ")]
         public string MovieTitle2 { get; set; }
+
+        [DisplayName("Movie Title 3")]
         public string MovieTitle3 { get; set; }
 
         // could add a viewModel with logic that allows any number of movies.  What data structure would work best?
